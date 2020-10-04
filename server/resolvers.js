6c966838d3bd02c92aacdf97ed1656f8549247fc
nijatb <nijatbakh@gmail.com>
2020-10-04 23:53:37 +0100

    setup graphql subscription on apollo server

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,7 @@
+const { PubSub } = require('graphql-subscriptions')
 const db = require('./db');
+
+const MESSAGE_ADDED = 'MESSAGE_ADDED'
 
 function requireAuth(userId) {
   if (!userId) {
@@ -17,8 +20,19 @@ const Mutation = {
   addMessage: (_root, {input}, {userId}) => {
     requireAuth(userId);
     const messageId = db.messages.create({from: userId, text: input.text});
-    return db.messages.get(messageId);
+    const message = db.messages.get(messageId);
+    pubSub.publish(MESSAGE_ADDED, {messageAdded: message });
+    return message;
   }
 }
 
-module.exports = { Query, Mutation };
+const pubSub =  new PubSub();
+
+const Subscription = {
+  messageAdded: {
+    subscribe: () => pubSub.asyncIterator(MESSAGE_ADDED)
+  }
+}
+
+
+module.exports = { Query, Mutation, Subscription };
